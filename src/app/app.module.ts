4bae37b9f7267fa23cb6fbadc86ed5f8a8b155b1
Dhanush V <dhanush.v0724@gmail.com>
2024-12-19 12:01:33 +0530

    voluteer form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-//added below new
-import { AngularFireModule } from '@angular/fire/compat'
+// Firebase Modules
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+// Environment Configuration
 import { environment } from '../environments/environment';
+
+// Components
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-import { FormsModule } from '@angular/forms';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
 import { ResourceComponent } from './component/resource/resource.component';
+import { VolunteerComponent } from './component/volunteer/volunteer.component';
+
+// Services
+import { RealtimeDatabaseService } from './realtime-database.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,17 +32,21 @@ import { ResourceComponent } from './component/resource/resource.component';
     DashboardComponent,
     ForgotPasswordComponent,
     VerifyEmailComponent,
-    ResourceComponent
+    ResourceComponent,
+    VolunteerComponent // Add VolunteerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    FormsModule
+    AngularFireModule.initializeApp(environment.firebase), // Firebase initialization
+    AngularFireAuthModule, // Firebase Auth
+    AngularFireDatabaseModule, // Firebase Realtime Database
+    FormsModule, // For template-driven forms
+    ReactiveFormsModule // For reactive forms
   ],
   providers: [
-    provideClientHydration()
+    RealtimeDatabaseService, // Add RealtimeDatabaseService
+    provideClientHydration() // For hydration
   ],
   bootstrap: [AppComponent]
 })
--- src/app/component/volunteer/volunteer.component.ts
+++ src/app/component/volunteer/volunteer.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+// src/app/component/volunteer/volunteer.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { RealtimeDatabaseService, Volunteer } from '../../realtime-database.service';
 
 @Component({
   selector: 'app-volunteer',
-  standalone: true,
-  imports: [],
   templateUrl: './volunteer.component.html',
-  styleUrl: './volunteer.component.css'
+  styleUrls: ['./volunteer.component.css'],
 })
-export class VolunteerComponent {
+export class VolunteerComponent implements OnInit {
+  volunteerForm!: FormGroup;
+  volunteers!: Observable<Volunteer[]>;
 
+  constructor(
+    private fb: FormBuilder,
+    private rtdbService: RealtimeDatabaseService
+  ) {}
+
+  ngOnInit(): void {
+    this.volunteerForm = this.fb.group({
+      name: ['', Validators.required],
+      number: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      capabilities: ['', Validators.required],
+      tagline: ['', Validators.required],
+    });
+
+    this.volunteers = this.rtdbService.getVolunteers();
+  }
+
+  onSubmit(): void {
+    if (this.volunteerForm.valid) {
+      const volunteerData: Volunteer = this.volunteerForm.value;
+      this.rtdbService
+        .addVolunteer(volunteerData)
+        .then(() => {
+          alert('Volunteer successfully registered!');
+          this.volunteerForm.reset();
+        })
+        .catch((error: any) => {
+          console.error('Error registering volunteer:', error);
+        });
+    }
+  }
 }
